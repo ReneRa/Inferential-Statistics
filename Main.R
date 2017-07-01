c8796a36c1f52e351bf6bfc806ce6385e5286eb2
ReneRa <rene.rauch@outlook.de>
2017-07-01 15:29:47 +0100

    Adding basic models

--- Main.R
+++ Main.R
@@ -1,4 +1,6 @@
 ###Inferential Statistics###
+
+
 rm(list = ls())
 juices = read.csv("JuiceDataset.csv", header = TRUE, sep = ";", row.names= 1)
 
@@ -18,5 +20,28 @@ try(if(all(abs(rowSums(juices[,13:24])-1)<0.001)) stop("Threshold for row sums")
 #Logit model
 logit <- glm(juices$Average_Recom ~ juices$Price + juices$Fruit_Proportion + juices$Fresh_Juice, family=binomial(link="logit"), data=juices)
 
+#Probit model
+probit <- glm(juices$Average_Recom ~ juices$Price + juices$Fruit_Proportion + juices$Fresh_Juice, family=binomial(link="probit"), data=juices)
+
+#Model Validity Test
+library(lmtest)
+lrtest()
+waldtest()
+
+
+#Multinomial Logit Model
+library(foreign)
+library(nnet)
+library(stargazer)
+MultinomLogit = multinom(Rounded_Average ~ Price + Fruit_Proportion + Fresh_Juice, data=juices)
+stargazer(MultinomLogit, type="html", out="multi1.htm")
+multi1.rrr = exp(coef(MultinomLogit))
+
+
 #notes:
 #http://www.princeton.edu/~otorres/LogitR101.pdf
+
+# Research question
+# 1. Impact of each fruit 
+# 2. 
+# 3.
