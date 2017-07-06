2f3c763ad432fc8cd413a263dcd0956be35a7e7b
Josefine Kuka <josefine.kuka@gmail.com>
2017-07-06 10:42:47 +0100

    Assumption Testing finalized, Linear model Avg_Rating finalized

--- Assumption_Testing.R
+++ Assumption_Testing.R
@@ -5,7 +5,7 @@
 ### H1: Linear in parameters - true
 
 ### H2: Test for Multicollinearity
-test_multicollinearity <- function(explanatory_data) {
+test_H2_multicollinearity <- function(explanatory_data, model) {
   # Normal Correlation Plot
   exp_cor <- cor(explanatory_data)
   corrplot(as.matrix(exp_cor), is.corr = FALSE, method = "color",
@@ -13,56 +13,61 @@ test_multicollinearity <- function(explanatory_data) {
   
   # Variance Inflation Factors
   vif <-vif(model) 
-  return(all(vif^2<2))
+  return(paste("VIF:",all(vif^2<4)))
 }
 
 ### H3: Zero Conditional Mean:
 #Strict Exogenity, Contemporaneous Exogeneity
-test_zeroConditionalMean <- function(explanatory_data, model) {
+test_H3_zeroConditionalMean <- function(explanatory_data, model) {
   # Error at time t uncorrelated with each explanatory variable in every time perio
   res <- model$residuals
   res_include_exp_data <- cbind(explanatory_data, res)
   # visual plotting of residuals against fitted values
   plot(model,1)
+  
+  
   # looking at the correlation
   res_cor <- cor(res_include_exp_data)
   corrplot(res_cor,is.corr = FALSE, method = "color",
-           order="hclust", type = "upper")
-  return (sum(res_cor==1)==ncol(res_cor))
+            type = "upper")
+  return ("Please check plot of fitted values against residuals")
 }
 
 # H4: Homoscedasticity
-test_homoscedasticity <- function (dependent_data, model, sig=0.05) {
+test_H4_homoscedasticity <- function (dependent_data, model, sig=0.05) {
   #Breusch-Pagan-Test: (if p<sig, reject H0 -> no homoscedasticity)
   bp_result <- bptest(model)
-  return (bp_result$p.value>sig)
+  return (paste("Breusch-Pagan-Test:",bp_result$p.value>sig))
 }
 
 ### H5 No serial correlation
-test_serialCorrelation <- function (model, sig=0.05) {
+test_H5_noSerialCorrelation <- function (model, sig=0.05) {
   # Durbin Watson: if p < sig, reject H0 -> serial correlation
   DurbinWatson_NoSerialCorr <- dwtest(model)$p.value > sig
-  return (DurbinWatson_NoSerialCorr)
+  return (paste("Durbin Watson Test:", DurbinWatson_NoSerialCorr))
 }
 
-# H6 Normal distribution of the errors
-test_errorNormality <- function (model, sig=0.1) {
+### H6 Normal distribution of the errors
+test_H6_NormalityOfErrors <- function (model, sig=0.1) {
+  result<-list()
   # QQ-Plot
   qqPlot(model$residuals)
-  
-  res <- model$residuals
-  normality <- shapiro.test(res)
-  return (normality$p.value < sig)
+  result$QQPlot <- "Please check QQ-Plot!"
+ 
+  #Shapiro Test
+  normality <- shapiro.test(model$residuals)
+  result$ShapiroTest<-(normality$p.value > sig)
+  return (result)
 } 
 
 ### Assumption Main
 assumptions_test <- function (model){
   result<- list()
-  result$H1 <- TRUE
-  result$H2 <- test_multicollinearity(juices[,2:24])
-  result$H3 <- test_zeroConditionalMean(juices[,2:24],model)
-  result$H4 <- test_homoscedasticity(juices[,2:24],model)
-  result$H5 <- test_serialCorrelation(model)
-  result$H6 <- test_errorNormality(model)
+  result$H1_LinearParameters <- TRUE
+  result$H2_multicollinearity <- test_H2_multicollinearity(juices[,2:24], model)
+  result$H3_zeroConditionalMean <- test_H3_zeroConditionalMean(juices[,2:24],model)
+  result$H4_homoscedasticity <- test_H4_homoscedasticity(juices[,2:24],model)
+  result$H5_noSerialCorrelation <- test_H5_noSerialCorrelation(model)
+  result$H6_errorNormality <- test_H6_NormalityOfErrors(model)
   return(result)
 }
--- Main.R
+++ Main.R
@@ -1,13 +1,10 @@
 ###Inferential Statistics###
 
-install.packages("globals")
+install.packages("stargazer")
 library("corrplot")
 library(lmtest)
 library(rms)
 library(car)
-library(globals)
-
-install.packages("rms")
 
 rm(list = ls())
 source("data_transformations.R")
@@ -18,8 +15,8 @@ juices = read.csv("JuiceDataset.csv", header = TRUE, sep = ";", row.names= 1)
 fruits = list("Apple","Orange","Pineapple","Mango","Grape","Maracuja","Peach","Pear","Banana","Carrot","Lemon","Others")
 common_fruits = list("Apple","Orange","Pineapple","Mango","Maracuja","Banana")
 other_fruits = c("Kiwi","Tomato","Grapefruit","Rosa_Camina","Guava","Apricot","Mandarin","Papaia","Raspberry","Blueberry","Strawberry","Cherries")
-pairs = list("BinApplePineapple", "BinAppleBanana", "BinAppleCarrot", "BinAppleOthers", "BinOrangeOthers", "BinOrangePineapple", "BinBananaPeach", "BinPearApple", "BinPearOrange")
-triples = list("AppOrangPina", "AppOrangBana", "AppOrangMan", "AppOrangMara", "OrangPinaMan", "OrangManMara")
+pairs = list("P_ApplePineapple", "P_AppleBanana", "P_AppleCarrot", "P_AppleOthers", "P_OrangeOthers", "P_OrangePineapple", "P_BananaPeach", "P_PearApple", "P_PearOrange")
+triples = list("T_AppOrangPina", "T_AppOrangBana", "T_AppOrangMan", "T_AppOrangMara", "T_OrangPinaMan", "T_OrangManMara")
 
 #New Targets
 #juices$Recom5 <- ifelse(juices$Average>=5,1,0)
@@ -42,12 +39,18 @@ juices_dummy<-fruitCombinations(juices_dummy,1,1)
 #Linear model
 copy_juices <- juices_scaled
 copy_juices$t<-juices$Added_Sugar * juices$Sugar
-copy_juices$Carrot2 <- copy_juices$Carrot^2*10
+copy_juices$SqCarrot <- copy_juices$Carrot^2*10
+copy_juices$SqLemon <- copy_juices$Lemon^2*10
 model1 = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + P_CarrotLemon+ P_PearApple"
-model2 = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + Carrot + Sugar + P_PearApple+ Kcal"
-model3 = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + Carrot + Added_Sugar + Fresh_Juice + P_PearApple"
-linear <- lm(as.formula(paste("Avg_Rating ~ ",model3,sep="")), data=copy_juices)
+model2 = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + Carrot + Carrot2 + Sugar + P_PearApple+ Kcal"
+model3 = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + Carrot + SqCarrot +  Added_Sugar + Fresh_Juice + P_PearApple"
+#model3_r = "T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan + Added_Sugar + Fresh_Juice + P_PearApple"
+linear <- lm(Avg_Rating ~ T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan +P_PearApple+ Carrot + SqCarrot +  Added_Sugar + Fresh_Juice, data=copy_juices)
+linear_r <- lm(Avg_Rating ~ Avg_Rating ~ T_AppOrangBana + T_AppOrangPina + T_OrangPinaMan +P_PearApple+  Added_Sugar + Fresh_Juice, data=copy_juices)
 summary(linear)
+assumptions_test(linear)
+waldtest(linear, linear_r)
+ 
 
 f<-paste(fruits[!fruits %in% common_fruits], collapse="+") #[ fruits %in% common_fruits]
 f<-""
@@ -72,7 +75,7 @@ copy_juices$Sugar2 = juices$Sugar^2
 copy_juices$SugarPerKcal = juices$Sugar/juices$Kcal
 copy_juices$Fruit_Proportion2 = juices$Fruit_Proportion^2
 #logit <- glm(as.formula(paste("Average_Recom~ Sugar + Sugar2 + BinPearApple + BinCarrotLemon",f,sep="")), family=binomial(link="probit"), data=copy_juices)
-logit <- glm(Average_Recom~ AppOrangBana+ OrangPinaMan + AppOrangPina + Kcal + Sugar + BinCarrotLemon+BinPeachApple, family=binomial(link="probit"), data=copy_juices)
+logit <- glm(Recommondation~ T_AppOrangBana+ T_OrangPinaMan + T_AppOrangPina + Kcal + Sugar + P_CarrotLemon+ P_PeachApple, family=binomial(link="probit"), data=copy_juices)
 summary(logit)
 #logit2 <- glm("Recom5 ~ ", family=binomial(link="logit"), data=juices$Average_Recom)
 #lrtest(logit, logit2)
